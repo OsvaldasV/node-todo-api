52519030fc93dc9a3fc127342e96fab718e9c8b3
OsvaldasV <Osvaldas.Vaivada@gmail.com>
2017-02-20 11:26:23 +0200

    Added seperate test database and refactored server.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // patikrina ar MONGODB_URI egzistuoja, jei ne tai naudoja localhost
 // MONGODB_URI ateina is mongoLab addono Heroku
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
