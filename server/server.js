285264a5425bbfe0da00a642a1235682a0250654
OsvaldasV <Osvaldas.Vaivada@gmail.com>
2017-02-21 17:15:15 +0200

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', authenticate, (req,res) => {
 	var todo = new Todo({
-		text: req.body.text
+		text: req.body.text,
+		_creator: req.user._id
 	});
 
 	todo.save().then((doc) =>{
@@ -27,16 +28,17 @@ app.post('/todos', (req,res) => {
 	});
 });
 
-app.get('/todos', (req,res) => {
-	
-	Todo.find().then((todos) =>{
+app.get('/todos', authenticate, (req,res) => {
+	Todo.find({
+		_creator: req.user._id
+	}).then((todos) =>{
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
 	});
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 
 	// req.params bus toks id koki ideda linke
 	// res.send(req.params);
@@ -47,7 +49,10 @@ app.get('/todos/:id', (req, res) => {
 		return res.status(404).send('Id not valid');
 	}
 	
-	Todo.findById(id).then((todo) => {
+	Todo.findOne({
+		_id: id,
+		_creator: req.user._id
+	}).then((todo) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
@@ -59,7 +64,7 @@ app.get('/todos/:id', (req, res) => {
 		
 });
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', authenticate, (req,res) => {
 	// get the id
 	var id = req.params.id;
 	if(!ObjectID.isValid(id)) {
@@ -67,7 +72,10 @@ app.delete('/todos/:id', (req,res) => {
 		return res.status(404).send('Id not valid');
 	}
 	// remove todo by id
-	Todo.findByIdAndRemove(id).then((todo) => {
+	Todo.findOneAndRemove({
+		_id: id,
+		_creator: req.user._id
+	}).then((todo) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
@@ -77,7 +85,7 @@ app.delete('/todos/:id', (req,res) => {
 	});
 });
 
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', authenticate, (req,res) => {
 	var id = req.params.id;
 	// pick is lodash bibliotekos paima tas properties, kuriam mum reikia, jei jos egzistuoja ir taip apsaugo, kad useris beleko neprivestu
 	var body = _.pick(req.body, ['text', 'completed']);
@@ -95,7 +103,10 @@ app.patch('/todos/:id', (req,res) => {
 	}
 	// set is mongodb operatoriu ateina
 	// pirmas argumentas id, antras argumentas values on our object, trecia argumentas yra options kaip kad mongodb-update returnOriginal : false ,kur yra, tai jis grazina new objekta be originalo, tai cia nustatom, kad grazintu new.
-	Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+	Todo.findOneAndUpdate({
+		_id: id,
+		_creator: req.user._id
+	}, {$set:body}, {new: true}).then((todo) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
